4b2e09a7a287ebcd870e0affc7b2dea3ba837fff
Krishna <krishnabiswakarmasc@gmail.com>
2022-06-30 12:28:40 +0530

    Search functionality of ambulance and covidtestcenter is enabled

--- src/Home/Details/Ambulance.js
+++ src/Home/Details/Ambulance.js
@@ -1,19 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Details.css";
 import Row from "./DetailRow/DetailRow";
 import AmbulanceData from "../../Data/AmbulanceData.json";
 
 function Details() {
+  const [searchTerm, setSearchTerm] = useState("");
   return (
     <div className="details">
       <div className="header">
         <p>AMBULANCE</p>
         <form action="">
-          <input type="text" id="input" placeholder="Search.." />
+          <input type="text" id="input" placeholder="Search.." onChange={(e) => setSearchTerm(e.target.value)} />
         </form>
       </div>
       <div className="details-body">
-        {AmbulanceData.map((item, index) => {
+        {AmbulanceData.filter((item) => {
+          if (searchTerm === "") {
+            return item;
+          } else {
+            return item.shop.toLowerCase().includes(searchTerm.toLowerCase());
+          }
+        }).map((item, index) => {
           return <Row key={index} shop={item.shop}
             address={item.address}
             number={item.number}
--- src/Home/Details/CovidTestCenter.js
+++ src/Home/Details/CovidTestCenter.js
@@ -1,19 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Details.css";
 import Row from "./DetailRow/DetailRow";
 import CovidTestCenterData from "../../Data/CovidTestCenterData.json";
 
 function Details() {
+  const [searchTerm, setSearchTerm] = useState("");
   return (
     <div className="details">
       <div className="header">
         <p>COVID TEST CENTER</p>
         <form action="">
-          <input type="text" id="input" placeholder="Search.." />
+          <input type="text" id="input" placeholder="Search.." onChange={(e) => setSearchTerm(e.target.value)} />
         </form>
       </div>
       <div className="details-body">
-        {CovidTestCenterData.map(item => (
+        {CovidTestCenterData.filter((item) => {
+          if (searchTerm === "") {
+            return item;
+          } else {
+            return item.shop.toLowerCase().includes(searchTerm.toLowerCase());
+          }
+        }).map(item => (
           <Row key={item.shop} shop={item.shop} address={item.address} number={item.number} />
         ))}
       </div>
