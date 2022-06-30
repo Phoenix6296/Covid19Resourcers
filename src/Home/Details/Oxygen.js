3bffad7a119151cd6b8354091c0481bb9a1e0a1d
Krishna <krishnabiswakarmasc@gmail.com>
2022-06-30 12:19:37 +0530

    Search functionality added to the oxygen file

--- src/Home/Details/Oxygen.js
+++ src/Home/Details/Oxygen.js
@@ -1,20 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Details.css";
 import Row from "./DetailRow/DetailRow";
 import OxygenData from "../../Data/OxygenData.json"
 
 function Details() {
+  const [searchTerm, setSearchTerm] = useState("");
   return (
     <div className="details">
       <div className="header">
         <p>OXYGEN</p>
         <form action="">
-          <input type="text" id="input" placeholder="Search.." />
+          <input type="text" id="input" placeholder="Search.." onChange={(e) => setSearchTerm(e.target.value)} />
         </form>
       </div>
       <div className="details-body">
 
-        {OxygenData.map((item, index) => {
+        {OxygenData.filter((item) => {
+          if (searchTerm === "") {
+            return item;
+          } else {
+            return item.shop.toLowerCase().includes(searchTerm.toLowerCase());
+          }
+        }).map((item, index) => {
           return <Row key={index} shop={item.shop}
             address={item.address}
             number={item.number}
